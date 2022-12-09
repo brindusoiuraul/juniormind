48c33c953db31b474de0dc25fd0c7585a8dbf6c8
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-09 18:13:16 +0200

    Finish integer and fraction
    Finish integer and fraction parts

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -6,7 +6,7 @@ namespace Json
     {
         public static bool IsJsonNumber(string input)
         {
-            if (!IsInputValid(input))
+            if (input == null)
             {
                 return false;
             }
@@ -14,72 +14,70 @@ namespace Json
             var indexOfDot = input.IndexOf('.');
             var indexOfExponent = input.IndexOfAny("eE".ToCharArray());
 
-            return IsInteger(Integer(input, indexOfDot, indexOfExponent));
+            return IsInteger(Integer(input, indexOfDot, indexOfExponent)) && IsFraction(Fraction(input, indexOfDot, indexOfExponent));
+        }
+
+        private static bool IsFraction(string input)
+        {
+            return input == string.Empty || ContainsOnlyDigits(input[1..]);
+        }
+
+        private static string Fraction(string input, int indexOfDot, int indexOfExponent)
+        {
+            if (indexOfDot == -1)
+            {
+                return string.Empty;
+            }
+
+            if (indexOfExponent != -1)
+            {
+                return input[indexOfDot.. indexOfExponent];
+            }
+
+            return input[indexOfDot.. input.Length];
         }
 
         private static bool IsInteger(string integer)
         {
-            bool isValidInteger = true;
-            const string validChars = "+-0123456789";
-
-            foreach (char c in integer)
+            if (integer.StartsWith('-'))
             {
-                if (!validChars.Contains(c))
-                {
-                    isValidInteger = false;
-                }
+                integer = integer[1..];
             }
 
             if (integer.Length > 1 && integer[0] == '0')
             {
-                isValidInteger = false;
+                 return false;
             }
 
-            return isValidInteger;
+            return ContainsOnlyDigits(integer);
         }
 
         private static string Integer(string input, int indexOfDot, int indexOfExponent)
         {
-            int integerEndIndex = GetEndIndex(input, indexOfDot, indexOfExponent);
-
-            return input.Substring(0, integerEndIndex + 1);
-        }
-
-        private static int GetEndIndex(string input, int indexOfDot, int indexOfExponent)
-        {
-            int integerEndIndex = input.Length - 1;
+            if (indexOfDot != -1)
+            {
+                return input[..indexOfDot];
+            }
 
             if (indexOfExponent != -1)
             {
-                integerEndIndex = indexOfExponent - 1;
+                return input[..indexOfExponent];
             }
 
-            if (indexOfDot != -1)
-            {
-                integerEndIndex = indexOfDot - 1;
-            }
-
-            return integerEndIndex;
+            return input;
         }
 
-        private static bool IsInputValid(string input)
+        private static bool ContainsOnlyDigits(string input)
         {
-            if (input == null)
+            foreach (var c in input)
             {
-                return false;
-            }
-
-            int numberOfDigits = 0;
-
-            for (int index = 0; index < input.Length; index++)
-            {
-                if (char.IsDigit(input[index]))
+                if (!char.IsDigit(c))
                 {
-                    numberOfDigits++;
+                    return false;
                 }
             }
 
-            return numberOfDigits > 0;
+            return input.Length > 0;
         }
     }
 }
