0d63877b10733b6be158f05ba6f71697f8bac923
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-09 18:35:10 +0200

    Finish
    Finish validator

--- Json.Start/Json.Validator/JsonNumber.cs
+++ Json.Start/Json.Validator/JsonNumber.cs
@@ -14,7 +14,32 @@ namespace Json
             var indexOfDot = input.IndexOf('.');
             var indexOfExponent = input.IndexOfAny("eE".ToCharArray());
 
-            return IsInteger(Integer(input, indexOfDot, indexOfExponent)) && IsFraction(Fraction(input, indexOfDot, indexOfExponent));
+            return
+                IsInteger(Integer(input, indexOfDot, indexOfExponent)) &&
+                IsFraction(Fraction(input, indexOfDot, indexOfExponent)) &&
+                IsExponent(Exponent(input, indexOfExponent));
+        }
+
+        private static bool IsExponent(string input)
+        {
+            const int exponentDigitsStart = 2;
+
+            if (input.Length > 2 && (input[1] == '+' || input[1] == '-'))
+            {
+                return ContainsOnlyDigits(input[exponentDigitsStart..]);
+            }
+
+            return input == string.Empty || ContainsOnlyDigits(input[1..]);
+        }
+
+        private static string Exponent(string input, int indexOfExponent)
+        {
+            if (indexOfExponent == -1)
+            {
+                return string.Empty;
+            }
+
+            return input[indexOfExponent..];
         }
 
         private static bool IsFraction(string input)
