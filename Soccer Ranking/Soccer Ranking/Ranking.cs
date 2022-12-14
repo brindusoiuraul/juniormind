ad3645cfe0b587b38bc89259976cc12d9408aab9
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-14 21:27:22 +0200

    Create project
    Create and upload starting project

---
+++ Soccer Ranking/Soccer Ranking/Ranking.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Soccer_Ranking
+{
+    public class Ranking
+    {
+
+    }
+}
---
+++ Soccer Ranking/Soccer Ranking/RankingFacts.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Soccer_Ranking
+{
+    public class RankingFacts
+    {
+        [Fact]
+        public void Test()
+        {
+
+        }
+    }
+}
