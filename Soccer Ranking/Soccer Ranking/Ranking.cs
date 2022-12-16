cecdd0798110a0194cd3f048ea3f8ac8717aed1e
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-16 15:18:48 +0200

    Create ranking constructor and functionality to add new team

--- Soccer Ranking/Soccer Ranking/Ranking.cs
+++ Soccer Ranking/Soccer Ranking/Ranking.cs
@@ -4,5 +4,17 @@ namespace SoccerRanking
 {
     public class Ranking
     {
+        private Team[] teams;
+
+        public Ranking(Team[] teams)
+        {
+            this.teams = teams;
+        }
+
+        public void AddTeamToRanking(Team teamToAdd)
+        {
+            Array.Resize(ref teams, teams.Length + 1);
+            teams[teams.Length - 1] = teamToAdd;
+        }
     }
 }
