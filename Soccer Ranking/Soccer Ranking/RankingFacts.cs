fda10bb3cba175c0cf4bde628a9e8b3cd43ca57f
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-16 16:41:14 +0200

    Add ranking tests

--- Soccer Ranking/Soccer Ranking/RankingFacts.cs
+++ Soccer Ranking/Soccer Ranking/RankingFacts.cs
@@ -5,8 +5,21 @@ namespace SoccerRanking
     public class RankingFacts
     {
         [Fact]
-        public void AddTeamToRanking()
+        public void AddTeamToRankingAddOneTeamShouldReturnTrue()
         {
+            Team[] teams =
+            {
+                new Team("Aerostar Bacau", 8),
+                new Team("CS Afumati", 5)
+            };
+
+            Ranking ranking = new Ranking(teams);
+
+            Team newTeam = new Team("CSM Deva", 12);
+
+            ranking.AddTeamToRanking(newTeam);
+
+            Assert.True(ranking.GetTeamPosition(newTeam) == 1);
         }
     }
 }
