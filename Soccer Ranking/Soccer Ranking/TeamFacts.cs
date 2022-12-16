22b47add59ebcc543432bdf4440227463c013d92
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-16 16:02:06 +0200

    Add team fact
    create test to check if a team has more points than other team should return false

--- Soccer Ranking/Soccer Ranking/TeamFacts.cs
+++ Soccer Ranking/Soccer Ranking/TeamFacts.cs
@@ -5,8 +5,12 @@ namespace SoccerRanking
     public class TeamFacts
     {
         [Fact]
-        public void Test()
+        public void HasMorePointsThan()
         {
+            Team firstTeam = new Team("CFR Cluj", 15);
+            Team secondTeam = new Team("Astra Giurgiu", 17);
+
+            Assert.False(firstTeam.HasMorePointsThan(secondTeam));
         }
     }
 }
