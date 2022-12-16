81be0cae5f509dc74fb2196bdd98163e438519fa
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-16 16:06:56 +0200

    Create tests
    Create new tests to check HasMorePointsThan function

--- Soccer Ranking/Soccer Ranking/TeamFacts.cs
+++ Soccer Ranking/Soccer Ranking/TeamFacts.cs
@@ -5,10 +5,28 @@ namespace SoccerRanking
     public class TeamFacts
     {
         [Fact]
-        public void HasMorePointsThan()
+        public void HasMorePointsThanCheckForTwoTeamsFirstTeamHasLessPointsThanSecondTeamShouldReturnFalse()
         {
             Team firstTeam = new Team("CFR Cluj", 15);
             Team secondTeam = new Team("Astra Giurgiu", 17);
+
+            Assert.False(firstTeam.HasMorePointsThan(secondTeam));
+        }
+
+        [Fact]
+        public void HasMorePointsThanCheckForTwoTeamsFirstTeamHasMorePointsThanSecondTeamShouldReturnTrue()
+        {
+            Team firstTeam = new Team("Steaua Bucuresti", 23);
+            Team secondTeam = new Team("Dinamo Bucuresti", 12);
+
+            Assert.True(firstTeam.HasMorePointsThan(secondTeam));
+        }
+
+        [Fact]
+        public void HasMorePointsThanCheckForTwoTeamsBothTeamPointsEqualShouldReturnFalse()
+        {
+            Team firstTeam = new Team("Gaz Metan Medias", 10);
+            Team secondTeam = new Team("Rapid", 10);
 
             Assert.False(firstTeam.HasMorePointsThan(secondTeam));
         }
