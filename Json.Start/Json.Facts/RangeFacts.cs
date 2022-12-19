0e16cc6296f04fa9cdd643e4fb822e81d8247a7c
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-19 23:47:43 +0200

    Create test
    Create new test to check if match function works properly when it should return true

--- Json.Start/Json.Facts/RangeFacts.cs
+++ Json.Start/Json.Facts/RangeFacts.cs
@@ -4,5 +4,14 @@ namespace Json.Facts
 {
     public class RangeFacts
     {
+        [Fact]
+        public void CheckMatchFunctionShouldReturnTrue()
+        {
+            var digit = new Range('a', 'f');
+
+            Assert.True(digit.Match("abc"));
+            Assert.True(digit.Match("fab"));
+            Assert.True(digit.Match("bcd"));
+        }
     }
 }
