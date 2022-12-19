9a9e78f9880893d108ddd6d04b41a3caa0048f0a
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-19 23:40:28 +0200

    Create Range.cs
    create constructor Range and Match function

---
+++ Json.Start/Json.Validator/Range.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Json
+{
+    public class Range
+    {
+        readonly char start;
+        readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            return start <= text[0] && text[0] <= end;
+        }
+    }
+}
