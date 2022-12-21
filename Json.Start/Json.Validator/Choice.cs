af8e97e5c9fc427daaae937d4df50c64d927f328
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-21 23:30:53 +0200

    Create Choice.cs

---
+++ Json.Start/Json.Validator/Choice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Json
+{
+    interface IPattern
+    {
+        bool Match(string pattern);
+    }
+
+    class Choice
+    {
+        public Choice(params IPattern[] patterns)
+        {
+        }
+
+        public bool Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
