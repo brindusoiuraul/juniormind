3c77eb135b7513fd803d1c98c91ab664d53e4ce9
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-28 19:20:55 +0200

    Create IMatch interface

---
+++ Json.Start/Json.Validator/IMatch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    internal interface IMatch
+    {
+        bool Succes();
+
+        string RemainingText();
+    }
+}
