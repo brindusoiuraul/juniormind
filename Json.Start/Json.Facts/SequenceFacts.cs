dd0805de57810b6ddb5557d440865fc49c07b6ea
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-30 20:38:32 +0200

    Add new tests
    Add new tests to check for a and b and new tests to check for a b and c

--- Json.Start/Json.Facts/SequenceFacts.cs
+++ Json.Start/Json.Facts/SequenceFacts.cs
@@ -19,6 +19,35 @@ namespace Json.Facts
 
             Assert.True(ab.Match("abcd").Success());
             Assert.Equal("cd", ab.Match("abcd").RemainingText());
+
+            Assert.False(ab.Match("ax").Success());
+            Assert.Equal("ax", ab.Match("ax").RemainingText());
+
+            Assert.False(ab.Match("def").Success());
+            Assert.Equal("def", ab.Match("def").RemainingText());
+
+            Assert.False(ab.Match("").Success());
+            Assert.Equal("", ab.Match("").RemainingText());
+
+            Assert.False(ab.Match("null").Success());
+            Assert.Equal("null", ab.Match("null").RemainingText());
+        }
+
+        [Fact]
+        public void CheckForABC()
+        {
+            var ab = new Sequence(
+                new Character('a'),
+                new Character('b')
+            );
+
+            var abc = new Sequence(
+                ab,
+                new Character('c')
+            );
+
+            Assert.True(abc.Match("abcd").Success());
+            Assert.Equal("d", abc.Match("abcd").RemainingText());
         }
     }
 }
