dea5d27950094e2530c17c526c076db82034d5f6
brindusoiuraul <brindusoiuraul@gmail.com>
2022-12-30 19:34:19 +0200

    Create Sequence.cs
    Create Sequence.cs

---
+++ Json.Start/Json.Validator/Sequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Sequence : IPattern
+    {
+        readonly IPattern[] patterns;
+
+        public Sequence(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
