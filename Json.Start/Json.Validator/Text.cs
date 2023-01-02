565f6cfc1c8b81f283dfca46864c0cbb5a9ea662
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-03 00:28:42 +0200

    Create Text.cs

---
+++ Json.Start/Json.Validator/Text.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Text : IPattern
+    {
+        public Text(string prefix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IMatch Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
