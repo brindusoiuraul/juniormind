fccb5f909666cb091b657a26bf4701702339df84
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-04 03:30:05 +0200

    Update ManyFacts.cs
    Add tests to check for existing patterns and non existing patterns, null and empty string

--- Json.Start/Json.Facts/ManyFacts.cs
+++ Json.Start/Json.Facts/ManyFacts.cs
@@ -17,5 +17,33 @@ namespace Json.Facts
             Assert.True(a.Match("abc").Success());
             Assert.Equal("bc", a.Match("abc").RemainingText());
         }
+
+        [Fact]
+        public void CheckForAAAABCShouldReturnTrueAndRemainingTextShouldBeBC()
+        {
+            Assert.True(a.Match("aaaabc").Success());
+            Assert.Equal("bc", a.Match("aaaabc").RemainingText());
+        }
+
+        [Fact]
+        public void CheckForNonExistingPatterShouldReturnTrueAndRemainingTextShouldBeBC()
+        {
+            Assert.True(a.Match("bc").Success());
+            Assert.Equal("bc", a.Match("bc").RemainingText());
+        }
+
+        [Fact]
+        public void CheckForEmptyStringShouldReturnTrueAndRemainingTextShouldBeAnEmptyString()
+        {
+            Assert.True(a.Match("").Success());
+            Assert.Equal("", a.Match("").RemainingText());
+        }
+
+        [Fact]
+        public void CheckForNullShouldReturnTrueAndRemainingTextShouldBeNull()
+        {
+            Assert.True(a.Match(null).Success());
+            Assert.Null(a.Match(null).RemainingText());
+        }
     }
 }
