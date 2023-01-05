b8d2bf01c6444cde5e5134865d373b612170a8b8
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-05 18:57:44 +0200

    Add new test
    Add new test to consume pattern

--- Json.Start/Json.Facts/OptionalFacts.cs
+++ Json.Start/Json.Facts/OptionalFacts.cs
@@ -9,5 +9,13 @@ namespace Json.Facts
 {
     public class OptionalFacts
     {
+        Optional a = new Optional(new Character('a'));
+
+        [Fact]
+        public void CheckForOneExistentPatternShouldReturnTrueAndRemainingTextShouldBeBC()
+        {
+            Assert.True(a.Match("abc").Success());
+            Assert.Equal("bc", a.Match("abc").RemainingText());
+        }
     }
 }
--- Json.Start/Json.Validator/Optional.cs
+++ Json.Start/Json.Validator/Optional.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Json
 {
-    class Optional : IPattern
+    public class Optional : IPattern
     {
         readonly IPattern pattern;
 
