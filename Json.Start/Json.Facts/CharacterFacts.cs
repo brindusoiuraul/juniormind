9919fae06b372b0880ae526c360b4e5fbbfbb958
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-06 01:30:15 +0200

    Create CharacterFacts.cs
    Create test file and add tests for Character class.

---
+++ Json.Start/Json.Facts/CharacterFacts.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Json.Facts
+{
+    public class CharacterFacts
+    {
+        Character x = new Character('x');
+
+        [Fact]
+        public void CheckForStringWithXAtTheStartShouldReturnTrue()
+        {
+            Assert.True(x.Match("xavier"));
+        }
+
+        [Fact]
+        public void CheckForStringWithoutXAtTheStartShouldReturnFalse()
+        {
+            Assert.False(x.Match("Fiora"));
+        }
+
+        [Fact]
+        public void CheckForNullShouldReturnFalse()
+        {
+            Assert.False(x.Match(null));
+        }
+
+        [Fact]
+        public void CheckForEmptyStringShouldReturnFalse()
+        {
+            Assert.False(x.Match(""));
+        }
+    }
+}
