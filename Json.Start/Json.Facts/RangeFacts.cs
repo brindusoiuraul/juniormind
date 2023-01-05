b409e3c3c6c5212bba8059cbc47a37effcfe83c5
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-06 01:01:36 +0200

    Update RangeFacts.cs
    Add tests.

--- Json.Start/Json.Facts/RangeFacts.cs
+++ Json.Start/Json.Facts/RangeFacts.cs
@@ -9,5 +9,42 @@ namespace Json.Facts
 {
     public class RangeFacts
     {
+        Range digit = new Range('a', 'f');
+
+        [Fact]
+        public void CheckForStringThatStartsWithAShouldReturnTrue()
+        {
+            Assert.True(digit.Match("abc"));
+        }
+
+        [Fact (Skip = "Locked test")]
+        public void CheckForStringThatStartsWithFShouldReturnTrue()
+        {
+            Assert.True(digit.Match("fab"));
+        }
+
+        [Fact (Skip = "Locked test")]
+        public void CheckForStringThatStartsWithBShouldReturnTrue()
+        {
+            Assert.True(digit.Match("bcd"));
+        }
+
+        [Fact (Skip = "Locked test")]
+        public void CheckForStringThatStartsWithANumberShouldReturnFalse()
+        {
+            Assert.False(digit.Match("1ab"));
+        }
+
+        [Fact (Skip = "Locked test")]
+        public void CheckForNullShouldReturnFalse()
+        {
+            Assert.False(digit.Match(null));
+        }
+
+        [Fact (Skip = "Locked test")]
+        public void CheckForEmptyStringShouldReturnFalse()
+        {
+            Assert.False(digit.Match(""));
+        }
     }
 }
