86c8a36339aaa86459d1fe7598c165f525f4d1ac
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-06 00:52:06 +0200

    Create Range.cs

---
+++ Json.Start/Json.Validator/Range.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Range
+    {
+        readonly char start;
+        readonly char end;
+
+        public Range(char start, char end)
+        {
+            this.start = start;
+            this.end = end;
+        }
+
+        public bool Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
