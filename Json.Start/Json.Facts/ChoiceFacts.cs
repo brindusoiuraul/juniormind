e1eca6dd4c945cb4b35b12847e3e5dc0008af1c4
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-07 02:25:20 +0200

    Update ChoiceFacts.cs
    Add tests

--- Json.Start/Json.Facts/ChoiceFacts.cs
+++ Json.Start/Json.Facts/ChoiceFacts.cs
@@ -19,5 +19,63 @@ namespace Json.Facts
         {
             Assert.True(digit.Match("012"));
         }
+
+        [Fact]
+        public void CheckStringWhenRangeMatchIsCorrectShouldReturnTrue()
+        {
+            Assert.True(digit.Match("12"));
+        }
+
+        [Fact]
+        public void CheckStringWhenRangeMatchIsCorrectButIsLastNumberFromTheRangeShouldReturnTrue()
+        {
+            Assert.True(digit.Match("92"));
+        }
+
+        [Fact]
+        public void CheckStringWithInexistentPatternShouldReturnFalse()
+        {
+            Assert.False(digit.Match("a9"));
+        }
+
+        [Fact]
+        public void CheckForEmptyStringShouldReturnFalse()
+        {
+            Assert.False(digit.Match(""));
+        }
+
+        [Fact]
+        public void CheckForNullShouldReturnFalse()
+        {
+            Assert.False(digit.Match(null));
+        }
+
+        [Fact]
+        public void CheckForCorrectCharPatternShouldReturnTrue()
+        {
+            Choice hex = new Choice(
+                digit,
+                new Choice(
+                    new Range('a', 'f'),
+                    new Range('A', 'F')
+                )
+            );
+
+            Assert.True(hex.Match("012"));
+        }
+
+        [Fact]
+        public void CheckForCorrectRangePatternUpperCaseLetters()
+        {
+            Choice hex = new Choice(
+                digit,
+                new Choice(
+                    new Range('a', 'f'),
+                    new Range('A', 'F')
+                )
+            );
+
+            Assert.True(hex.Match("A9"));
+        }
     }
 }
