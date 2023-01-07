39f97b20b7abe8b436115dbbbdb6818d8ba825ae
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-07 02:05:30 +0200

    Create Choice class and IPattern interface

---
+++ Json.Start/Json.Validator/Choice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Choice
+    {
+        readonly IPattern[] patterns;
+
+        public Choice(params IPattern[] patterns)
+        {
+            this.patterns = patterns;
+        }
+
+        public bool Match(string text)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
