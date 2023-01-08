585474640ba2389a073f316ce72848b017585e0c
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-08 02:48:40 +0200

    Create SequenceFacts.cs
    Create test file for Sequence class

---
+++ Json.Start/Json.Facts/SequenceFacts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Json.Facts
+{
+    public class SequenceFacts
+    {
+        Sequence ab = new Sequence(
+            new Character('a'),
+            new Character('b')
+        );
+
+        [Fact]
+        public void CheckForStringWhichSequenceOfPatternsIsCorrectShouldReturnTrue()
+        {
+            Assert.True(ab.Match("abcd").Success());
+            Assert.Equal("cd", ab.Match("abcd").RemainingText());
+        }
+    }
+}
