a8177213f985e1e3ccd4a883677312c9737e1c02
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-09 01:46:21 +0200

    Create OnerOrMore.cs

---
+++ Json.Start/Json.Validator/OneOrMore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class OneOrMore
+    {
+        readonly IPattern pattern;
+
+        public OneOrMore(IPattern pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
