ece2ecc7e671a6f286ee234fbd8ab94794da2c5f
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-09 14:37:27 +0200

    Create List.cs

---
+++ Json.Start/Json.Validator/List.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class List : IPattern
+    {
+        readonly IPattern pattern;
+
+        public List(IPattern element, IPattern separator)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
