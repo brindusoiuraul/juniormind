c33a6ca70b25fdcdf825dffaa744f6d282fe8e3d
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-11 19:10:45 +0200

    Add functionality
    Add new tests and add functionality to return true if a string is empty but is double quoted

--- Json.Start/Json.Facts/StringFacts.cs
+++ Json.Start/Json.Facts/StringFacts.cs
@@ -10,17 +10,59 @@ namespace Json.Facts
     public class StringFacts
     {
         [Fact]
-        public void CheckForCorrectJSONStringShouldReturnTrue()
+        public void CheckForCorrectJSONStringJustLowerCaseShouldReturnTrue()
         {
             var jsonString = new StringJson();
             Assert.True(jsonString.Match(Quoted("xyz")).Success());
         }
 
         [Fact]
-        public void CheckForCorrectJSONStringUsingJustUpperCaseLetters()
+        public void CheckForCorrectJSONStringUsingJustUpperCaseLettersShouldReturnTrue()
         {
             var jsonString = new StringJson();
             Assert.True(jsonString.Match(Quoted("XYZ")).Success());
+        }
+
+        [Fact]
+        public void CheckForCorrectJSONStringUsingLowerCaseAndUpperCaseLettersShouldReturnTrue()
+        {
+            var jsonString = new StringJson();
+            Assert.True(jsonString.Match(Quoted("xYz")).Success());
+        }
+
+        [Fact]
+        public void CheckIfDoesNotStartWithQuotesShouldReturnFalse()
+        {
+            var jsonString = new StringJson();
+            Assert.False(jsonString.Match("xyz\"").Success());
+        }
+
+        [Fact]
+        public void CheckIfDoesNotEndsWithQuotesShouldReturnFalse()
+        {
+            var jsonString = new StringJson();
+            Assert.False(jsonString.Match("\"xyz").Success());
+        }
+
+        [Fact]
+        public void CheckForNullShouldReturnFalse()
+        {
+            var jsonString = new StringJson();
+            Assert.False(jsonString.Match(null).Success());
+        }
+
+        [Fact]
+        public void CheckForEmptyStringShouldReturnFalse()
+        {
+            var jsonString = new StringJson();
+            Assert.False(jsonString.Match(string.Empty).Success());
+        }
+
+        [Fact]
+        public void CheckForEmptyDoubleQuotedStringShouldReturnTrue()
+        {
+            var jsonString = new StringJson();
+            Assert.True(jsonString.Match(Quoted(string.Empty)).Success());
         }
 
         public string Quoted(string text)
--- Json.Start/Json.Validator/String.cs
+++ Json.Start/Json.Validator/String.cs
@@ -15,8 +15,10 @@ namespace Json
             var doubleQuote = new Character('"');
             var lowerCaseLetter = new Range('a', 'z');
             var upperCaseLetter = new Range('A', 'Z');
+
             var letters = new OneOrMore(new Choice(lowerCaseLetter, upperCaseLetter));
-            this.pattern = new Sequence(doubleQuote, letters, doubleQuote);
+
+            this.pattern = new Sequence(doubleQuote, new OptionalJson(letters), doubleQuote);
         }
 
         public IMatch Match(string text)
