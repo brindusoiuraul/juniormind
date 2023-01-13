98ee2e069e5fb77aaaca7d4a754122ae925162ed
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-13 13:14:11 +0200

    Create Value.cs

---
+++ Json.Start/Json.Validator/Value.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Value : IPattern
+    {
+        readonly IPattern pattern;
+
+        public Value()
+        {
+            this.pattern = new StringJson();
+        }
+
+        public IMatch Match(string text)
+        {
+            return pattern.Match(text);
+        }
+    }
+}
