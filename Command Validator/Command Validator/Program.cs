cb40547ad7169e15a80c640a03e03f37029ec8cb
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-23 00:23:22 +0200

    Create Project
    Create Command Validator Project

---
+++ Command Validator/Command Validator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CommandValidator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
