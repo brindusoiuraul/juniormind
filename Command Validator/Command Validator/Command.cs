3f6b8de61ad4ac22eb8394560d47cc01e9981243
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-23 15:21:59 +0200

    Create Command.cs

---
+++ Command Validator/Command Validator/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command_Validator
+{
+    public class Command
+    {
+        readonly string argument;
+        readonly string value;
+
+        public Command(string command, string value)
+        {
+            this.argument = command;
+            this.value = value;
+        }
+    }
+}
