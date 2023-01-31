7e1dcb20690e1f58ab3c55d534936e16afc99756
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-31 02:05:07 +0200

    Import classes
    Import classes from json validator project

---
+++ ArgumentsValidator/ArgumentsValidator/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Json
+{
+    public class Character : IPattern
+    {
+        readonly char pattern;
+
+        public Character(char pattern)
+        {
+            this.pattern = pattern;
+        }
+
+        public IMatch Match(string text)
+        {
+            return
+                !string.IsNullOrEmpty(text) && text[0] == pattern ?
+                new Match(true, text[1..]) :
+                new Match(false, text);
+        }
+    }
+}
