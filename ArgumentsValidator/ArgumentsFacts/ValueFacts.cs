7e2510f946682e8c9520ccc4cea7e1a0454385b7
brindusoiuraul <brindusoiuraul@gmail.com>
2023-01-31 02:32:40 +0200

    Add value functionality

---
+++ ArgumentsValidator/ArgumentsFacts/ValueFacts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArgumentsValidator
+{
+    public class ValueFacts
+    {
+        [Fact]
+        public void CheckForCorrectValueShouldReturnTrue()
+        {
+            var value = new Value();
+
+            Assert.True(value.Match("abc").Success());
+            Assert.Equal("", value.Match("abc").RemainingText());
+        }
+
+        [Fact]
+        public void CheckForInvalidValueShouldReturnFalse()
+        {
+            var value = new Value();
+            Assert.False(value.Match(null).Success());
+        }
+    }
+}
