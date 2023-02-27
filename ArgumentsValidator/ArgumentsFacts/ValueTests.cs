3bdcaec3d3c66678dff624f5de4940c3e69622c2
brindusoiuraul <brindusoiuraul@gmail.com>
2023-02-27 15:45:23 +0200

    Create ValueTests.cs
    Create test file for Value class and add a test which should pass

---
+++ ArgumentsValidator/ArgumentsFacts/ValueTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ArgumentsValidator
+{
+    public class ValueTests
+    {
+        [Fact]
+        public void CheckForCorrectValueShouldReturnTrueAndShouldNotRemainAnything()
+        {
+            Value value = new Value();
+            string[] args = { "raul" };
+            Assert.True(value.CheckArg(args).Success());
+            Assert.Equal(new string[0], value.CheckArg(args).RemainingArguments());
+        }
+    }
+}
