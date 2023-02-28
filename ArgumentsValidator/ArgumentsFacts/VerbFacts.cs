56a6439c5345679eb443bf7672bf9eec4a89df0c
brindusoiuraul <brindusoiuraul@gmail.com>
2023-02-28 12:26:09 +0200

    Verb

--- ArgumentsValidator/ArgumentsFacts/VerbFacts.cs
+++ ArgumentsValidator/ArgumentsFacts/VerbFacts.cs
@@ -9,5 +9,40 @@ namespace ArgumentsValidator
 {
     public class VerbFacts
     {
+        [Fact]
+        public void CheckForValidVerbShouldReturnTrue()
+        {
+            Verb verb = new Verb(new Argument(new Value()));
+            string[] arguments = { "git", "--message", "abc" };
+
+            Assert.True(verb.CheckArg(arguments).Success());
+        }
+
+        [Fact]
+        public void CheckForInvalidSimpleVerbShouldReturnFalse()
+        {
+            Verb verb = new Verb(new Argument());
+            string[] arguments = { "git", "--message", "abc" };
+
+            Assert.False(verb.CheckArg(arguments).Success());
+        }
+
+        [Fact]
+        public void CheckValidVerbWithMultipleArgumentsShouldReturnTrue()
+        {
+            Verb verb = new Verb(new Argument(new Value()), new Argument());
+            string[] arguments = { "git", "--message", "abc", "--ammend" };
+
+            Assert.True(verb.CheckArg(arguments).Success());
+        }
+
+        [Fact]
+        public void CheckValidVerbWithMultipleArgumentsAndMultipleValuesShouldReturnTrue()
+        {
+            Verb verb = new Verb(new Argument(new Value()), new Argument(new Value()));
+            string[] arguments = { "git", "--message", "abc", "--ammend", "source" };
+
+            Assert.True(verb.CheckArg(arguments).Success());
+        }
     }
 }
--- ArgumentsValidator/ArgumentsValidator/Verb.cs
+++ ArgumentsValidator/ArgumentsValidator/Verb.cs
@@ -6,7 +6,33 @@ using System.Threading.Tasks;
 
 namespace ArgumentsValidator
 {
-    public class Verb
+    public class Verb : IArguments
     {
+        readonly Argument[] argumentsPattern;
+        readonly IPattern pattern;
+
+        public Verb(params Argument[] argumentsPattern)
+        {
+            this.argumentsPattern = argumentsPattern;
+            this.pattern = new OneOrMore(new Range('a', 'z'));
+        }
+
+        public ICheck CheckArg(string[] arguments)
+        {
+            if (!pattern.Match(arguments[0]).Success())
+            {
+                return new Check(false, arguments[1..]);
+            }
+
+            foreach (Argument argument in argumentsPattern)
+            {
+                if (!argument.CheckArg(arguments).Success())
+                {
+                    return new Check(false, arguments);
+                }
+            }
+
+            return new Check(true, arguments);
+        }
     }
 }
