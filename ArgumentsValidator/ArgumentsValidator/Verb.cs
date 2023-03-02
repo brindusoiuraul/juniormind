b3e9bd9a54cb4c9487335b8a030051149c848d08
brindusoiuraul <brindusoiuraul@gmail.com>
2023-03-03 00:30:03 +0200

    Create Verb.cs

---
+++ ArgumentsValidator/ArgumentsValidator/Verb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgumentsValidator
+{
+    public class Verb
+    {
+        readonly string name;
+        readonly string argument;
+
+        public Verb(string name, string argument)
+        {
+            this.name = name;
+            this.argument = argument;
+        }
+    }
+}
