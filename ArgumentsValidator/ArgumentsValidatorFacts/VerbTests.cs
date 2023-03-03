d8edc58fbe6fffc6636c00c43d7d054668159a22
brindusoiuraul <brindusoiuraul@gmail.com>
2023-03-03 14:05:52 +0200

    Update VerbTests.cs
    Add tests to check

--- ArgumentsValidator/ArgumentsValidatorFacts/VerbTests.cs
+++ ArgumentsValidator/ArgumentsValidatorFacts/VerbTests.cs
@@ -5,8 +5,12 @@ namespace ArgumentsValidator
     public class VerbTests
     {
         [Fact]
-        public void CheckForValidVerbShouldReturnTrue()
+        public void CheckForValidVerbWithOneArgumentShouldReturnTrue()
         {
+            Verb verb = new Verb("commit", new Argument("message", "m"));
+            string[] commands = { "commit", "--message" };
+
+            Assert.True(verb.Match(commands).Success());
         }
     }
 }
