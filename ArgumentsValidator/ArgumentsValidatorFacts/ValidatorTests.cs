bfcaf9e45e906b1ea61520bf919f81c8b1994a76
brindusoiuraul <brindusoiuraul@gmail.com>
2023-03-06 01:06:39 +0200

    Update ValidatorTests.cs
    Finish tests

--- ArgumentsValidator/ArgumentsValidatorFacts/ValidatorTests.cs
+++ ArgumentsValidator/ArgumentsValidatorFacts/ValidatorTests.cs
@@ -18,5 +18,20 @@ namespace ArgumentsValidator
             Assert.True(validator.Match(correctCommitCommand).Success());
             Assert.True(validator.Match(correctCloneCommand).Success());
         }
+
+        [Fact]
+        public void CheckForTwoMoreComplexVerbs()
+        {
+            var commit = new Verb("commit", new Argument("message", "m", new Argument("setcolor", "s", new Value("red"), new Value("green"), new Value("blue"))));
+            var clone = new Verb("clone", new Argument("all", "a"));
+
+            string[] commitCommand = { "commit", "--message", "--setcolor", "green" };
+            string[] cloneCommand = { "clone", "-a" };
+
+            Validator validator = new Validator(commit, clone);
+
+            Assert.True(validator.Match(commitCommand).Success());
+            Assert.True(validator.Match(cloneCommand).Success());
+        }
     }
 }
