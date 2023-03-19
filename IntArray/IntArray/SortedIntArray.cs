bafda4dc5647f3ad6bd70b05105ce3ecee901323
brindusoiuraul <brindusoiuraul@gmail.com>
2023-03-19 23:24:35 +0200

    Implement Heapsort And Override Add method

--- IntArray/IntArray/SortedIntArray.cs
+++ IntArray/IntArray/SortedIntArray.cs
@@ -12,5 +12,60 @@ namespace IntArray
                 base[index] = value;
             }
         }
+
+        public override void Add(int element)
+        {
+            Count++;
+            this.Insert(Count - 1, element);
+            Sort();
+        }
+
+        private void Sort()
+        {
+            int size = Count;
+
+            for (int index = size / 2 - 1; index >= 0; index--)
+            {
+                Heapify(size, index);
+            }
+
+            for (int index = size - 1; index >= 0; index--)
+            {
+                Swap(0, index);
+                Heapify(index, 0);
+            }
+        }
+
+        private void Heapify(int size, int index)
+        {
+            int largest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+
+            if (left < size && base[left] > base[largest])
+            {
+                largest = left;
+            }
+
+            if (right < size && base[right] > base[largest])
+            {
+                largest = right;
+            }
+
+            if (largest == index)
+            {
+                return;
+            }
+
+            Swap(index, largest);
+            Heapify(size, largest);
+        }
+
+        private void Swap(int firstIndex, int secondIndex)
+        {
+            int temp = base[firstIndex];
+            base[firstIndex] = base[secondIndex];
+            base[secondIndex] = temp;
+        }
     }
 }
--- IntArray/IntArrayTests/SortedIntArrayTests.cs
+++ IntArray/IntArrayTests/SortedIntArrayTests.cs
@@ -16,5 +16,20 @@ namespace IntArray
             array[0] = 1;
             Assert.Equal(1, array[0]);
         }
+
+        [Fact]
+        public void CheckIfElementsAreSortedAfterAdd()
+        {
+            var array = new SortedIntArray();
+            array.Add(5);
+            array.Add(2);
+            array.Add(4);
+            array.Add(4);
+
+            Assert.Equal(2, array[0]);
+            Assert.Equal(4, array[1]);
+            Assert.Equal(4, array[2]);
+            Assert.Equal(5, array[3]);
+        }
     }
 }
