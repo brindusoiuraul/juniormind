4ac00e2d7cecd3673bc083d326242f75c9375578
brindusoiuraul <brindusoiuraul@gmail.com>
2023-03-28 20:05:49 +0300

    Add test to check for add method on dif data types

--- IntArray/IntArrayTests/ObjectArrayTests.cs
+++ IntArray/IntArrayTests/ObjectArrayTests.cs
@@ -11,5 +11,21 @@ namespace IntArray
             ObjectArray objectArray = new ObjectArray();
             Assert.Equal(0, objectArray.Count);
         }
+
+        [Fact]
+        public void CheckForAddMethodWithDifferentDataTypes()
+        {
+            ObjectArray objectArray = new ObjectArray();
+
+            objectArray.Add(5);
+            objectArray.Add(15.34);
+            objectArray.Add("Raul");
+            objectArray.Add('c');
+
+            Assert.Equal(5, objectArray[0]);
+            Assert.Equal(15.34, objectArray[1]);
+            Assert.Equal("Raul", objectArray[2]);
+            Assert.Equal('c', objectArray[3]);
+        }
     }
 }
