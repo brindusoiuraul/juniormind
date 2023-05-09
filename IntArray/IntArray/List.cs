b16f672df19fcc136f6fb3c85ca8f06a2b79c39f
brindusoiuraul <brindusoiuraul@gmail.com>
2023-05-10 02:55:45 +0300

    Add get, add and set functionality

--- IntArray/IntArray/List.cs
+++ IntArray/IntArray/List.cs
@@ -15,7 +15,7 @@ namespace IntArray
 
         public int Count { get; set; }
 
-        public T this[int index]
+        public virtual T this[int index]
         {
             get => this[index];
             set => this[index] = value;
@@ -34,7 +34,7 @@ namespace IntArray
             return GetEnumerator();
         }
 
-        public void Add(T element) => Insert(Count, element);
+        public virtual void Add(T element) => Insert(Count, element);
 
         public bool Contains(T element) => IndexOf(element) != -1;
 
