a2cdce58fcf91c34d9fb8369ab87955ce583a02e
brindusoiuraul <brindusoiuraul@gmail.com>
2023-10-31 18:37:04 +0200

    Revert "Reset"
    This reverts commit 139e1a49a70c3fe6e518aff5875f742077a3cdab.

--- IntArray/IntArrayTests/SortedListTests.cs
+++ IntArray/IntArrayTests/SortedListTests.cs
@@ -5,5 +5,16 @@ namespace IntArray
 {
     public class SortedListTests
     {
+        [Fact]
+        public void TestGet()
+        {
+            var list = new SortedList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            Assert.Equal(2, list[1]);
+        }
     }
 }
