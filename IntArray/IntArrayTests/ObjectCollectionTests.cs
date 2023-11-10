d8ba1eb689ef287fcef76186778254fe07d38cc5
brindusoiuraul <brindusoiuraul@gmail.com>
2023-11-10 15:54:30 +0200

    Rename files after classes

--- IntArray/IntArrayTests/ObjectCollectionTests.cs
+++ IntArray/IntArrayTests/ObjectCollectionTests.cs
@@ -4,7 +4,7 @@ using Xunit;
 
 namespace IntArray
 {
-    public class ObjectArrayTests
+    public class ObjectCollectionTests
     {
         [Fact]
         public void TestForCountWhenTheArrayHasNoElements()
