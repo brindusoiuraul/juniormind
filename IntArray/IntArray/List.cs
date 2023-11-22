78390b9955d7bcdd72969c9dc0c89e51070c0cda
brindusoiuraul <brindusoiuraul@gmail.com>
2023-11-22 15:26:21 +0200

    Implement List<T> and first test

---
+++ IntArray/IntArray/List.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace IntArray
+{
+    public class List<T> : IEnumerable<T>
+    {
+        private T[] objectArray;
+
+        public List()
+        {
+            objectArray = new T[4];
+        }
+
+        public int Count { get; private set; }
+
+        public T this[int index]
+        {
+            get => objectArray[index];
+            set => objectArray[index] = value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return objectArray[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T element) => Insert(Count, element);
+
+        public bool Contains(T element) => IndexOf(element) != -1;
+
+        public int IndexOf(T element)
+        {
+            for (int index = 0; index < Count; index++)
+            {
+                if (objectArray[index].Equals(element))
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+
+        public void Insert(int index, T element)
+        {
+            Count++;
+            EnlargeArray();
+            ShiftRight(index);
+            objectArray[index] = element;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            Array.Resize(ref objectArray, 4);
+        }
+
+        public bool Remove(T element)
+        {
+            int index = IndexOf(element);
+
+            if (index == -1)
+            {
+                return false;
+            }
+
+            RemoveAt(index);
+            return true;
+        }
+
+        public void RemoveAt(int index)
+        {
+            ShiftLeft(index);
+            Count--;
+            Array.Resize(ref objectArray, objectArray.Length - 1);
+        }
+
+        private void EnlargeArray()
+        {
+            if (Count < objectArray.Length)
+            {
+                return;
+            }
+
+            Array.Resize(ref objectArray, objectArray.Length * 2);
+        }
+
+        private void ShiftRight(int index)
+        {
+            for (int objectArrayIndex = objectArray.Length - 1; objectArrayIndex > index; objectArrayIndex--)
+            {
+                objectArray[objectArrayIndex] = objectArray[objectArrayIndex - 1];
+            }
+        }
+
+        private void ShiftLeft(int startIndex)
+        {
+            for (int objectArrayIndex = startIndex; objectArrayIndex < objectArray.Length - 1; objectArrayIndex++)
+            {
+                objectArray[objectArrayIndex] = objectArray[objectArrayIndex + 1];
+            }
+        }
+    }
+}
---
+++ IntArray/IntArrayTests/ListTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IntArrayTests
+{
+    public class ListTests
+    {
+        [Fact]
+        public void CheckForLengthWhenListHasNoElements()
+        {
+            List<int> list = new List<int>();
+            Assert.Equal(0, list.Count);
+        }
+    }
+}
