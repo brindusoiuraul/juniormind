ea9d111e5e120337a31fb75ff71bb65bee7efc81
brindusoiuraul <brindusoiuraul@gmail.com>
2023-12-14 16:52:29 +0200

    Add exception for trying to modify the list when the list is read-only

--- IntArray/IntArrayTests/ListTests.cs
+++ IntArray/IntArrayTests/ListTests.cs
@@ -148,5 +148,14 @@ namespace IntArray
             var exception = Assert.Throws<ArgumentException>(() => nameList.CopyTo(newArray, 0));
             Assert.Equal("Not enough space.", exception.Message);
         }
+
+        [Fact]
+        public void TryChangeElementsWhenTheListIsReadOnly()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3 };
+
+            var exception = Assert.Throws<NotSupportedException>(() => numbers[2] = 1);
+            Assert.Equal("The list is Read-Only", exception.Message);
+        }
     }
 }
