63b445abe41c9e3ebce4fb41c04e2713df462e60
brindusoiuraul <brindusoiuraul@gmail.com>
2024-02-27 18:37:07 +0200

    Upload main functions blueprint

--- GzipAndCrypt/GzipAndCrypt/GzipCrypt.cs
+++ GzipAndCrypt/GzipAndCrypt/GzipCrypt.cs
@@ -4,9 +4,19 @@ namespace GzipAndCrypt
 {
     public class GzipCrypt
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public void Write(Stream stream, string text, bool compress = false, bool encrypt = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(Stream stream, bool compress = false, bool encrypt = false)
+        {
+            throw new NotImplementedException();
         }
     }
 }
