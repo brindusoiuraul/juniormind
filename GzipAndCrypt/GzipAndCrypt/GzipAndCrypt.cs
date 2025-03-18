1f99abb91a6d9f89ec1a90ce69ffa356e82e0ecd
brindusoiuraul <brindusoiuraul@gmail.com>
2025-03-18 15:23:27 +0200

    Starting code
    First files

---
+++ GzipAndCrypt/GzipAndCrypt/GzipAndCrypt.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GzipAndCrypt
+{
+    public class GzipAndCrypt
+    {
+        public GzipAndCrypt()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GzipAndCrypt/GzipAndCryptTests/GzipAndCryptTests.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace GzipAndCrypt
+{
+    public class GzipAndCryptTests
+    {
+        [Fact]
+        public void Test()
+        {
+        }
+    }
+}
