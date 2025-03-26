364625fd37f3dd97181deb59de3a660049534a1e
brindusoiuraul <brindusoiuraul@gmail.com>
2025-03-26 14:11:28 +0200

    Create skeleton

---
+++ GzipAndCrypt/GzipAndCrypt/GzipAndCrypt.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace GzipAndCrypt
+{
+    public class GzipAndCrypt
+    {
+        public void Write(Stream stream, string text, bool encrypt = false, bool compress = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(Stream stream, bool encrypted = false, bool compressed = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
