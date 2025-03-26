9045497833ec01c5b9ef7357d96c08c4e88bf347
brindusoiuraul <brindusoiuraul@gmail.com>
2025-03-26 17:17:06 +0200

    Upload skeleton

---
+++ GzipAndCrypt/GzipAndCrypt/GzipAndCrypt.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Net.Security;
+
+namespace GzipAndCrypt
+{
+    public class GzipAndCrypt
+    {
+        public GzipAndCrypt()
+        {
+        }
+
+        public void Write(Stream stream, string text, bool encrypt = false, bool compress = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(Stream stream, bool encrypt = false, bool compress = false)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GzipAndCrypt/GzipAndCryptTests/GzipAndCryptTests.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace GzipAndCrypt
+{
+    public class GzipAndCryptTests
+    {
+    }
+}
