7cfd776bd39221db71baa7be8b00a5111f9dfa8d
brindusoiuraul <brindusoiuraul@gmail.com>
2025-03-31 00:23:29 +0300

    Restart project but without the ruleset

--- GZipAndCrypt/GZipAndCrypt/GZipAndCrypt.cs
+++ GZipAndCrypt/GZipAndCrypt/GZipAndCrypt.cs
@@ -1,71 +1,9 @@
-using System.IO.Compression;
-using System.Security.Cryptography;
-using System.Text;
-using static System.Net.Mime.MediaTypeNames;
-
 namespace GZipAndCrypt
 {
     public class GZipAndCrypt
     {
         public GZipAndCrypt()
         {
-        }
-
-        public void Write(Stream stream, string input, bool compress = false, bool encrypt = false)
-        {
-            ArgumentNullException.ThrowIfNull(stream);
-
-            ValidateInput(input);
-
-            if (compress)
-            {
-                using var compressedStream = new GZipStream(stream, CompressionMode.Compress, leaveOpen: true);
-                WriteToStream(compressedStream, input);
-            }
-            else
-            {
-                WriteToStream(stream, input);
-            }
-
-            stream.Position = 0;
-        }
-
-        public string Read(Stream stream, bool compressed = false, bool encrypted = false)
-        {
-            ArgumentNullException.ThrowIfNull(stream);
-
-            if (compressed)
-            {
-                using var deprocessedStream = new GZipStream(stream, CompressionMode.Decompress);
-
-                return ReadFromStream(deprocessedStream);
-            }
-
-            return ReadFromStream(stream);
-        }
-
-        private static void WriteToStream(Stream stream, string input)
-        {
-            byte[] dataBytes = Encoding.UTF8.GetBytes(input);
-
-            stream.Write(dataBytes, 0, dataBytes.Length);
-            stream.Flush();
-        }
-
-        private static string ReadFromStream(Stream stream)
-        {
-            using var reader = new StreamReader(stream);
-            return reader.ReadToEnd();
-        }
-
-        private static void ValidateInput(string input)
-        {
-            if (!string.IsNullOrEmpty(input))
-            {
-                return;
-            }
-
-            throw new ArgumentException("Input text cannot be null or empty!", nameof(input));
         }
     }
 }
