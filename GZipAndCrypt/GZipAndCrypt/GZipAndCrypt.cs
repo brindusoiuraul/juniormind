9a406780a4a502421cadb63894c94e401d5fcf1a
brindusoiuraul <brindusoiuraul@gmail.com>
2025-03-31 00:25:57 +0300

    Upload skeleton

--- GZipAndCrypt/GZipAndCrypt/GZipAndCrypt.cs
+++ GZipAndCrypt/GZipAndCrypt/GZipAndCrypt.cs
@@ -5,5 +5,15 @@ namespace GZipAndCrypt
         public GZipAndCrypt()
         {
         }
+
+        public void Write(Stream stream, string input, bool compress = false, bool encrypt = false)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Read(Stream stream, bool compressed = false, bool encrypted = false)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
